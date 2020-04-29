ed3293b5202bf1d7fc6e2828c4bd3ed5e1f57d54
DUSHGAUTAM <64513655+DUSHGAUTAM@users.noreply.github.com>
2020-04-29 09:04:50 +0530

    Added Code

---
+++ Parking.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ParkingApp
+{
+    partial class Parking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPark = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.ddlType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ddlParking = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtVehicleNo = new System.Windows.Forms.TextBox();
+            this.lblCountPark = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPark
+            // 
+            this.btnPark.Location = new System.Drawing.Point(166, 229);
+            this.btnPark.Name = "btnPark";
+            this.btnPark.Size = new System.Drawing.Size(99, 39);
+            this.btnPark.TabIndex = 0;
+            this.btnPark.Text = "Park";
+            this.btnPark.UseVisualStyleBackColor = true;
+            this.btnPark.Click += new System.EventHandler(this.btnPark_Click);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(442, 229);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(91, 39);
+            this.Close.TabIndex = 1;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // ddlType
+            // 
+            this.ddlType.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ddlType.FormattingEnabled = true;
+            this.ddlType.Location = new System.Drawing.Point(210, 53);
+            this.ddlType.Name = "ddlType";
+            this.ddlType.Size = new System.Drawing.Size(252, 33);
+            this.ddlType.TabIndex = 2;
+            this.ddlType.SelectedIndexChanged += new System.EventHandler(this.ddlType_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Vehicle Type :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Available Parking :";
+            // 
+            // ddlParking
+            // 
+            this.ddlParking.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ddlParking.FormattingEnabled = true;
+            this.ddlParking.Location = new System.Drawing.Point(210, 110);
+            this.ddlParking.Name = "ddlParking";
+            this.ddlParking.Size = new System.Drawing.Size(252, 33);
+            this.ddlParking.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(84, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Vehicle No :";
+            // 
+            // txtVehicleNo
+            // 
+            this.txtVehicleNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVehicleNo.Location = new System.Drawing.Point(210, 161);
+            this.txtVehicleNo.Name = "txtVehicleNo";
+            this.txtVehicleNo.Size = new System.Drawing.Size(252, 30);
+            this.txtVehicleNo.TabIndex = 7;
+            // 
+            // lblCountPark
+            // 
+            this.lblCountPark.AutoSize = true;
+            this.lblCountPark.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountPark.Location = new System.Drawing.Point(468, 113);
+            this.lblCountPark.Name = "lblCountPark";
+            this.lblCountPark.Size = new System.Drawing.Size(103, 25);
+            this.lblCountPark.TabIndex = 8;
+            this.lblCountPark.Text = "Available :";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.Location = new System.Drawing.Point(577, 112);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 25);
+            this.lblCount.TabIndex = 9;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(304, 229);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 39);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Parking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 334);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblCountPark);
+            this.Controls.Add(this.txtVehicleNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ddlParking);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ddlType);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.btnPark);
+            this.Name = "Parking";
+            this.Text = "Parking Status";
+            this.Load += new System.EventHandler(this.Parking_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPark;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.ComboBox ddlType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ddlParking;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtVehicleNo;
+        private System.Windows.Forms.Label lblCountPark;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Parking.cs
@@ -0,0 +1,178 @@
+﻿using ParkingApp.BLL;
+using ParkingApp.EntityClasses;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace ParkingApp
+{
+    public partial class Parking : Form
+    {
+        List<Vehicle> vehList = new List<Vehicle>();
+        BusinessLogic objBAL = new BusinessLogic();
+        int typeId = 0;
+        string tableName = "";
+        public Parking()
+        {
+            InitializeComponent();
+        }
+
+        private void Parking_Load(object sender, EventArgs e)
+        {
+            bindVehicleType();
+            lblCountPark.Visible = false;
+        }
+
+        void bindVehicleType()
+        {
+            try
+            {
+                DataTable dtVeh = new DataTable();
+                dtVeh = objBAL.getVehicleType();
+                ddlType.DataSource = dtVeh;
+                ddlType.DisplayMember = "Name";
+                ddlType.ValueMember = "Id";
+            }
+            catch
+            {
+                //MessageBox.Show("Exception : ");
+            }
+        }
+
+        void checkParking(string s)
+        {
+            try
+            {
+                lblCountPark.Visible = false;
+                DataTable dtPark = new DataTable();
+                dtPark = objBAL.getPakingSlot(s);
+                int count = dtPark.Rows.Count;
+                if(count>0)
+                    lblCountPark.Visible = true;
+                lblCount.Text = count.ToString();
+                if(count==0)
+                {
+                    if(typeId>4)
+                    {
+                        MessageBox.Show("Sorry, No parking available at this time.");
+                        return;
+                    }
+                    MessageBox.Show("Selected type parking is not available, we are searching for you another available parking.");
+                    typeId++;
+                    ddlType.SelectedIndex = typeId;
+                    //string str = GetProperParking(typeId);
+                    //checkParking(str);
+                }
+                else
+                {
+                    ddlParking.DataSource = dtPark;
+                    ddlParking.ValueMember = "Id";
+                    ddlParking.DisplayMember = "SlotNo";
+                }
+            }
+            catch
+            {
+                //MessageBox.Show("Exception : ");
+            }
+        }
+
+        private void ddlType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string type = ddlType.Text.Trim();
+            typeId = ddlType.SelectedIndex;
+
+            if(type!="Select" && type != "System.Data.DataRowView")
+            {
+                string str = GetProperParking(typeId);
+                checkParking(str);
+            }
+        }
+
+        string GetProperParking(int type)
+        {
+            string ParkingType="";
+            switch(type)
+            {
+                case 1:
+                    ParkingType = "ParkingSmall";
+                    break;
+
+                case 2:
+                    ParkingType = "ParkingMedium";
+                    break;
+
+                case 3:
+                    ParkingType = "ParkingLarge";
+                    break;
+            }
+            tableName = ParkingType;
+            return ParkingType;
+        }
+
+        private void btnPark_Click(object sender, EventArgs e)
+        {
+            if(ddlType.SelectedIndex<1)
+            {
+                MessageBox.Show("Please select the vehicle type.");
+                return;
+            }
+            if(txtVehicleNo.Text.Trim().Length<1)
+            {
+                MessageBox.Show("Please enter vehicle number.");
+                return;
+            }
+
+            DateTime currTime = new DateTime();
+            currTime = DateTime.Now;
+            string updateQry = "Update " + tableName + " Set IsAvailable=0, VehicleNo='" + txtVehicleNo.Text.Trim() + "', EntryTime=GetDate() where SlotNo='" + ddlParking.Text.Trim() + "'";
+            string parkQuery = "Insert into ParkingHistory(VehicleNo, ParkingSlot, EntryTime)Values('" + txtVehicleNo.Text.Trim() + "','" + ddlParking.Text.Trim() + "',GetDate());" + updateQry;
+            bool status = objBAL.SaveDetail(parkQuery);
+            if (status)
+            {
+                MessageBox.Show("Parked Successfully.");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if(ddlType.Text=="Select")
+            {
+                ParkingHistory parkingDetail = new ParkingHistory();
+                if(txtVehicleNo.Text.Trim().Length>0)
+                {
+                    parkingDetail = objBAL.GetVehicleDetail(txtVehicleNo.Text.Trim());
+                    if (parkingDetail.ParkingSlot != null)
+                    {
+                        string type = parkingDetail.ParkingSlot.Substring(0, 1);
+                        if (type == "S")
+                            typeId = 1;
+                        if (type == "M")
+                            typeId = 2;
+                        if (type == "L")
+                            typeId = 3;
+
+                        GetProperParking(typeId);
+                        string updateQry = "Update ParkingHistory Set ExitTime=GETDATE()  where VehicleNo='"+ parkingDetail.VehicleNo+ "' and Id='"+ parkingDetail.Id+ "'";
+                        string exitParkQuery = "Update " + tableName + " Set ExitTime=GETDATE(), IsAvailable=1 where VehicleNo='" + parkingDetail.VehicleNo + "' and SlotNo='"+ parkingDetail.ParkingSlot+ "';" + updateQry;
+                        bool status = objBAL.SaveDetail(exitParkQuery);
+                        if (status)
+                        {
+                            MessageBox.Show("Thank you for visiting.");
+                            txtVehicleNo.Clear();
+                        }
+                    }
+                    else
+                        MessageBox.Show("This vehicle does not exist, Please provide the valid vehicle number.");
+                }
+                else
+                    MessageBox.Show("Please provide the vehicle number.");
+            }
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
